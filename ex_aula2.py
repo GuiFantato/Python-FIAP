63196270cd278254ddb281c8975e3f7d74793a1b
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-09 20:59:40 -0300

    Aula2

---
+++ ex_aula2.py
@@ -0,0 +1,53 @@
+from mpmath import convert
+
+# Exercicio 1
+nome = "Guilherme Fantato"
+print(f"Ex.1 {nome} \n")
+
+# Exercicio 2
+a = 3
+b = 5
+calc = (2 * a) * (3 * b)
+print(f"Ex.2: {calc} \n")
+
+# # Exercicio 3
+# v1 = 7
+# v2 = 9
+# v3 = 10
+# calc2 = v1 + v2 + v3
+# print(f"Ex.3: {calc2} \n")
+#
+# media = calc2/3
+# print(f"O número com duas casas decimais é {media:.2f} \n")
+#
+# # Exercício 4
+# v4 = int(input("Digite o valor 1: "))
+# v5 = int(input("Digite o valor 2: "))
+# sum_ex4 = v4 + v5
+# print(f"Soma dos valores: {v4 + v5} \n")
+#
+# # Exercício 5
+# metro = float(input("Digite quantos metros você quer converter para milímetro:"))
+# milimetro = metro * 1000
+# print(f"A conversão de {metro} metros para milímetros corresponde a: {milimetro} milimetros")
+
+# # Exercício 6
+# dias = int(input("Quantos dias? "))
+# horas = int(input("Quantas horas? "))
+# minutos = int(input("Quantos minutos? "))
+# segundos = int(input("Quantos segundos? "))
+# soma_segundos = (dias*24*60*60) + (horas*60*60) + (minutos*60) + segundos
+# print(f"Esse valor em segundos é: {soma_segundos}")
+
+# # Exercício 7
+# salario = float(input("Digite o seu salário: "))
+# porcentagem = float(input("Digite a porcentagem do aumento (sem o simbolo de %): "))
+# aumento = salario + (salario/100)*porcentagem
+# print(f"Seu salário com o aumento ficará: {aumento}")
+
+# # Exercicio 8
+preco = float(input("Digite o preço da mercadoria: "))
+desconto = float(input("Digite a porcentagem do desconto (sem o simbolo de %): "))
+valor_desconto = (preco/100)*desconto
+valor_total = ()
+print(valor_desconto)
---
+++ segunda_aula.py
@@ -0,0 +1,4 @@
+check1 = float(input("Nota 1: "))
+check2 = float(input("Nota 2: "))
+new_media = (check1 + check2) / 2
+print(f"Média: {new_media:.1f}")
